7cac6ff693625769a6c1fed6dc787b277cd503fc
tertudev <wendelgomesdh7@gmail.com>
2026-01-03 08:19:15 -0400

    feat(cars): add search and ordering to cars view

--- cars/views.py
+++ cars/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    request.GET.get('search')
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search: 
+        cars = Car.objects.filter(model__icontains=search)
 
     return render(
         request, 
