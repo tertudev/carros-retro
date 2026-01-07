0cba6005877908003be6177ad0ca0915a7295b77
tertudev <wendelgomesdh7@gmail.com>
2026-01-07 12:18:50 -0400

    feat(forms): add validations for car value and factory_year

--- cars/forms.py
+++ cars/forms.py
@@ -5,3 +5,25 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+
+        if value is not None and value < 20000:
+            self.add_error(
+                'value',
+                'O valor do carro não pode ser menor que R$20.000'
+            )
+
+        return value
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+
+        if factory_year is not None and factory_year < 1975:
+            self.add_error(
+                'factory_year',
+                'O ano de fabricação não pode ser menor que 1975'
+            )
+
+        return factory_year
