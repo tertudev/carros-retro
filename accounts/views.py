ede446eb4690b931c1b165848ac3e1b5f1b77770
tertudev <wendelgomesdh7@gmail.com>
2026-01-09 19:08:29 -0400

    feat(accounts): add user registration view

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+def register_view(request):
+    user_form = UserCreationForm()
+    return render(
+        request, 
+        'register.html', 
+        {'user_form': user_form
+    })
