7749fb1a899c7ce0c1cad89bb8fba71037d0d7fc
tertudev <wendelgomesdh7@gmail.com>
2026-03-01 19:46:39 -0400

    refactor(views): replace custom car creation view with Django generic CreateView

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 class CarsListView(ListView):
     model = Car
@@ -16,22 +14,8 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(
-            request,
-            'new_car.html',
-            {'new_car_form': new_car_form}
-        )
-
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(
-            request,
-            'new_car.html',
-            {'new_car_form': new_car_form}
-        )
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
