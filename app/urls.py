d62dd43cf0ca1dff7b02016c5921fcd396442afd
tertudev <wendelgomesdh7@gmail.com>
2026-03-02 18:42:02 -0400

    feat(urls): implement car detail page using DetailView

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsListView, NewCarCreateView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView
 from accounts.views import register_view, login_view, logout_view
 
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
+    path('cars/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
