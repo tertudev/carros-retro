422c42568df0cbfdd755cb644e1b623976bbe645
tertudev <wendelgomesdh7@gmail.com>
2026-03-03 13:44:49 -0400

    feat(urls): add car delete route to urlpatterns

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView, CarDeleteView
 from accounts.views import register_view, login_view, logout_view
 
 
@@ -15,4 +15,5 @@ urlpatterns = [
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
     path('cars/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
     path('cars/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
+    path('cars/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
