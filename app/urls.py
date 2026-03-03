b1ca91a748bac42b66788238f457dcb866c955a7
tertudev <wendelgomesdh7@gmail.com>
2026-03-03 12:52:42 -0400

    feat(urls): implement car update functionality using Django UpdateView

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsListView, NewCarCreateView, CarDetailView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView
 from accounts.views import register_view, login_view, logout_view
 
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
     path('cars/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('cars/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
