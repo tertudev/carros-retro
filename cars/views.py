61378198913639bd7b80098f78d128f39d63c0f3
tertudev <wendelgomesdh7@gmail.com>
2026-03-04 14:55:55 -0400

    feat(views): enforce authentication on create, update and delete views

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,8 @@
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 class CarsListView(ListView):
@@ -15,16 +17,18 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
 
-class CarDetailView(DetailView):
-    model = Car
-    template_name = 'car_detail.html'
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -33,6 +37,7 @@ class CarUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
