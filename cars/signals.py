40b092579d26b793620fc4ac0534f8db6e467b5a
tertudev <wendelgomesdh7@gmail.com>
2026-03-12 00:47:56 -0400

    feat(signals): integrate AI service to auto-generate car bio on pre_save

--- cars/signals.py
+++ cars/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ai_bio
 
 def car_inventory_update():
     cars_count = Car.objects.all().count()
@@ -17,7 +18,12 @@ def car_inventory_update():
 @receiver(pre_save, sender=Car)
 def car_pre_save(sender, instance, **kwargs):
     if not instance.bio:
-        instance.bio = 'Bio gerada automaticamente!'
+        ai_bio = get_car_ai_bio(
+            instance.model,
+            instance.brand,
+            instance.model_year
+        )
+        instance.bio = ai_bio
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
